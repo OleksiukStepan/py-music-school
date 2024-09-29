1a305cba2c5abf8a6fb1b70de6c28bd9b3be9630
Stepan <oleksiukstepann@gmail.com>
2024-09-29 22:00:36 +0300

    update models.py & urls.py

--- musician/models.py
+++ musician/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -5,22 +6,12 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
     instrument = models.CharField(max_length=63)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(14)])
     date_of_applying = models.DateTimeField(auto_now_add=True)
 
     @property
     def is_adult(self):
         return self.age >= 21
-
-    def clean(self):
-        if not 14 <=self.age:
-            raise ValueError({
-                "age": "age must be upper then 13"
-            })
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        return super().save()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
